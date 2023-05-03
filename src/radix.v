daaeb27f955e5c782735df03d2a93336242be151
valomay <valomay@protonmail.com>
2023-05-03 09:33:14 +0200

    add radix proof

--- src/radix.v
+++ src/radix.v
@@ -1,22 +1,18 @@
 Require Import ZArith.
 Require Import tree.
 
-Module RADIX <: TREE.
-  Definition elt := positive.
-  Definition elt_eq := Pos.eq_dec.
+Open Scope positive_scope.
 
-  Inductive tree (A: Type) : Type :=
-  | Empty: tree A
-  | Node:  tree A -> option A -> tree A -> tree A.
+Section Radix.
+  Context {A: Type}.
 
-  Definition t := tree.
+  Inductive tree : Type :=
+  | Empty: tree
+  | Node:  tree -> option A -> tree -> tree.
 
-  Arguments Empty {A}.
-  Arguments Node  {A} _ _.
-
-  Definition empty (A: Type) : tree A := Empty.
+  Definition empty : tree := Empty.
   
-  Fixpoint get {A} (p: positive) (t: tree A) : option A :=
+  Fixpoint get (p: positive) (t: tree) : option A :=
     match p, t with
     | _,     Empty      => None
     | xH,    Node _ e _ => e
@@ -24,14 +20,14 @@ Module RADIX <: TREE.
     | xI p', Node _ _ r => get p' r
     end.
 
-  Fixpoint set0 {A} (p: positive) (e: A) : tree A :=
+  Fixpoint set0 (p: positive) (e: A) : tree :=
     match p with
     | xH    => Node Empty (Some e) Empty
     | xO p' => Node (set0 p' e) None Empty
     | xI p' => Node Empty None (set0 p' e)
     end.
 
-  Fixpoint set {A} (p: positive) (e: A) (t: tree A) : tree A :=
+  Fixpoint set (p: positive) (e: A) (t: tree) : tree :=
     match p, t with
     | _,     Empty      => set0 p e
     | xH,    Node l _ r => Node l (Some e) r
@@ -40,18 +36,51 @@ Module RADIX <: TREE.
     end.
 
   Theorem gempty:
-    forall (A: Type) (i: elt), get i (empty A) = None.
+    forall (i: positive), get i (empty) = None.
   Proof.
-    intros A i. case i; reflexivity.
+    intro i. case i; reflexivity.
   Qed.
 
-  Theorem gss:
-    forall (A: Type) (i: elt) (x: A) (t: t A), get i (set i x t) = Some x.
-  Proof. Admitted.
-End RADIX.
+  Theorem gss0:
+    forall (i: positive) (x: A), get i (set0 i x) = Some x.
+  Proof.
+    intros i x. induction i; simpl; auto.  
+  Qed.
 
-Open Scope positive_scope.
+  Theorem gos0:
+    forall (i j: positive) (x: A), 
+    i <> j -> get i (set0 j x) = None.
+  Proof.
+    induction i; intros j x H; simpl.
+    - destruct j; simpl; [apply IHi; now intros <-| |]; apply gempty.
+    - destruct j; simpl; [| apply IHi; now intros <-|]; apply gempty.
+    - destruct j; now simpl.
+  Qed.
 
-Let radix := RADIX.empty positive.
-Let radix2 := RADIX.set 2 2 radix.
-Compute RADIX.get 2 radix2.
+
+  Theorem gss: 
+    forall (i: positive) (x: A) (t: tree), get i (set i x t) = Some x.
+  Proof.
+    induction i; destruct t; simpl; auto; try apply IHi; now rewrite gss0.
+  Qed.
+
+  Theorem gso:
+    forall (i j: positive) (x: A) (t: tree),
+    i <> j -> get i (set j x t) = get i t.
+  Proof.
+    induction i; intros j x t H; destruct j, t;
+    try auto using go0s; try apply gempty; try easy.
+    - apply gos0. assumption.
+    - apply IHi. now intros <-.
+    - apply gos0. assumption.
+    - apply IHi. now intros <-.
+  Qed.
+
+End Radix.
+
+Arguments tree A : clear implicits.
+
+Let radix': tree positive := empty.
+Let radix'2 := set 2 2 radix'.
+Compute get 2 radix'2.
+
