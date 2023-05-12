b263e9136f4cd5c3cb4c9cb0f9b934a740ca2edb
valomay <valomay@protonmail.com>
2023-05-12 21:31:32 +0200

    add some proof

--- src/hashtable.v
+++ src/hashtable.v
@@ -6,7 +6,6 @@ Require Import Lia.
 Require Import Coq.Lists.List.
 Import ListNotations.
 Require Import Coq.Numbers.Cyclic.Int63.Uint63.
-(* Require Import Coq.Numbers.Cyclic.Int63.PrimInt63. *)
 
 Open Scope uint63_scope.
 Print int.
@@ -39,7 +38,7 @@ Section Hashtable.
     now case eq_spec.
   Qed.
 
-  Definition bucket := list (A * B).
+  Definition bucket := list (int * A * B).
   Definition table := PArray.array bucket.
   
   Record t : Set := hash_tab { 
@@ -50,9 +49,11 @@ Section Hashtable.
   Fixpoint power_2_above' (n: nat) (x p: int) {struct n} : int :=
     match n with
     | O    => p
-    | S n' => if (p <? x) 
-             then power_2_above' n' x (p * 2)
-             else p
+    | S n' => 
+        if (p <? x) then 
+          power_2_above' n' x (p * 2)
+        else 
+          p
     end.
   
   (* 2**22 - 1 = max_length *)
@@ -63,15 +64,19 @@ Section Hashtable.
     let h := make size [] in
     hash_tab 0 h.
 
+  Definition key_index (h: t) (k: int) : int :=
+    k land PArray.length (hashtab h) - 1.
 
-  Definition key_index (h: t) (k: A) : int :=
-    (hash k) land (PArray.length (hashtab h) - 1).
+  Lemma key_index_max:
+    forall (h: t) (k: int),
+    (to_Z (key_index h k) < to_Z (PArray.length (hashtab h)))%Z.
+  Proof.
+    admit.
+  Admitted.
 
-  Definition get_bucket (h: t) (k: A) : bucket :=
-    if PArray.length (hashtab h) =? 0 then
-      []
-    else 
-      (hashtab h).[key_index h k].
+  Definition get_bucket (h: t) (k: int) : bucket :=
+    if PArray.length (hashtab h) =? 0 then []
+    else (hashtab h).[key_index h k].
 
   Definition resize_heurisitic (h: t) : bool :=
     PArray.length (hashtab h) =? 0.
@@ -88,57 +93,71 @@ Section Hashtable.
     admit.
   Admitted.
 
+  Lemma length_resize_non_neg:
+    forall (h: t),
+    (PArray.length (hashtab (resize h))) <> 0.
+  Proof.
+    admit.
+  Admitted.
+
+
   Definition add (h: t) (key: A) (v: B) : t :=
     let h := resize h in
     let tab := hashtab h in
-    let i := key_index h key in
+    let hash := hash key in
+    let i := key_index h hash in
     let bucket := get tab i in
-    let new := (key, v) :: tab.[i] in
+    let new := (hash, key, v) :: tab.[i] in
     hash_tab (size h + 1) tab.[i<-new].
 
-  Fixpoint find_rec (l: bucket) (key: A) : option B :=
+  Fixpoint find_rec (l: bucket) (h: int) (k: A): option B :=
     match l with
     | [] => None
-    | (k', v) :: l' => if eq k' key then Some v else find_rec l' key
+    | (h', k', v) :: l' => 
+        if andb (h =? h') (eq k k')
+        then Some v 
+        else find_rec l' h k
     end.
 
-  Fixpoint find_all_rec (l: bucket) (key: A) (acc: list B): list B :=
+  Fixpoint find_all_rec (l: bucket) (h: int) (k: A) (acc: list B): list B :=
     match l with
     | [] => List.rev' acc
-    | (k', v) :: l' => 
-        if eq k' key 
-        then find_all_rec l' key (v :: acc)
-        else find_all_rec l' key acc
+    | (h', k', v) :: l' => 
+        if andb (h =? h') (eq k k')
+        then find_all_rec l' h k (v :: acc)
+        else find_all_rec l' h k acc
     end.
 
-  Fixpoint find_all_rec' (l: bucket) (key: A) : list B :=
+  Fixpoint find_all_rec' (l: bucket) (h: int) (k: A) : list B :=
     match l with
     | [] => []
-    | (k', v) :: l' => 
-        if eq k' key 
-        then v :: find_all_rec' l' key
-        else find_all_rec' l' key
+    | (h', k', v) :: l' => 
+        if andb (h =? h') (eq k k')
+        then v :: find_all_rec' l' h k
+        else find_all_rec' l' h k
     end.
 
   Lemma find_all_rec_correct:
-    forall (l: bucket) (key: A),
-    find_all_rec l key [] = find_all_rec' l key.
+    forall (l: bucket) (h: int) (key: A),
+    find_all_rec l h key [] = find_all_rec' l h key.
   Proof.
-    intros l key. 
-    change (find_all_rec l key [] = rev [] ++ find_all_rec' l key).
+    intros l h k. 
+    change (find_all_rec l h k [] = rev [] ++ find_all_rec' l h k).
     generalize (nil : list B). induction l.
     + intros acc. simpl. apply rev_append_rev.
-    + intros acc. simpl. destruct a. case eq.
+    + intros acc. simpl. destruct a, p. case (_ && _).
       rewrite IHl. simpl. rewrite <- app_assoc. easy.
       apply IHl.
   Qed.
 
   Definition find (h: t) (key: A) : option B :=
-    let bucket := get_bucket h key in
-    find_rec bucket key.
+    let hash := hash key in
+    let bucket := get_bucket h hash in
+    find_rec bucket hash key.
 
   Definition find_all (h: t) (key: A) : list B :=
-    find_all_rec (get_bucket h key) key [].
+    let hash := hash key in
+    find_all_rec (get_bucket h hash) hash key [].
 
   Lemma find_all_resize:
     forall (h: t) (k: A),
@@ -153,16 +172,18 @@ Section Hashtable.
 
   Theorem hempty:
     forall (k: A) (size: int), find (create size) k = None.
-  Proof. Admitted.
-  (*   intros k i. unfold create, find, get_bucket. simpl. *)
-  (*   rewrite length_make. *)
-  (*   now rewrite get_make. *)
-  (* Qed. *)
+  Proof.
+    intros k i. unfold create, find, get_bucket. simpl.
+    rewrite length_make.
+    case (power_2_above i 16 <=? max_length).
+    case eqb. now simpl. now rewrite get_make.
+    simpl. now rewrite get_make.
+  Qed.
 
   Lemma key_index_eq:
     forall (h1 h2: t) (k: A), 
     PArray.length (hashtab h1) = PArray.length (hashtab h2) ->
-    key_index h1 k = key_index h2 k.
+    key_index h1 (hash k) = key_index h2 (hash k).
   Proof.
     intros h1 h2 k H. unfold key_index.
     now rewrite H.
@@ -170,7 +191,7 @@ Section Hashtable.
 
   Theorem dadd:
     forall (k: A) (v: B) (h: t),
-    find_all (add h k v) k = v::(find_all h k).
+    find_all (add h k v) k = v :: (find_all h k).
   Proof.
     intros k v h. rewrite <- (find_all_resize h).
     unfold find_all.
@@ -179,10 +200,12 @@ Section Hashtable.
     rewrite 2!eqb_false_complete.
     rewrite (key_index_eq (add h k v) (resize h)).
     unfold add; simpl. rewrite get_set_same.
-    simpl. rewrite eq_refl. easy.
-    admit.
+    simpl. rewrite eqb_refl. rewrite eq_refl. easy.
+    rewrite ltb_spec. apply key_index_max.
     unfold add; simpl. apply length_set.
-  Admitted.
+    2:unfold add; simpl; rewrite length_set.
+    all:apply length_resize_non_neg.
+  Qed.
 
 End Hashtable.
 
